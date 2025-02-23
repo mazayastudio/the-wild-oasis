d75846f4b611d3a2ef6570b316a70c10ae8028b0
sayudha <mazaya.studio@outlook.com>
2025-02-23 19:27:19 +0700

    Integrate React Query and Supabase for data fetching
    Added React Query for state management with `QueryClientProvider` wrapping the app and enabled devtools. Integrated Supabase with a new client for API interaction, including a `getCabins` function for fetching cabin data, enhancing the data-fetching capabilities of the app.

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,40 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import Applayout from "./ui/Applayout.jsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000
+    }
+  }
+});
 
 function App() {
-  return (<>
-    <GlobalStyles />
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Applayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
-        </Route>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Applayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>;
-  </>);
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import {createClient} from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jqgwthdcwdrjifqngjym.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZ3d0aGRjd2RyamlmcW5nanltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzMDMwMDUsImV4cCI6MjA1NTg3OTAwNX0.tdcWpcB9NNpc5ZNFhU6uZZAGzKwbbxGC1N93gEqbvIw';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

d003d11c2b2f34478623ce3892eb835c1bc5d5f9
sayudha <mazaya.studio@outlook.com>
2025-02-26 07:17:59 +0700

    Add modal functionality for creating new cabins
    Refactor cabin creation by introducing a reusable `Modal` component and moving the cabin form logic to a dedicated `AddCabin` component. Simplifies `Cabins` page by replacing inline form handling with the new modular structure.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,10 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
-import Button from "../ui/Button.jsx";
-import {useState} from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
+import AddCabin from "../features/cabins/AddCabin.jsx";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
+
 
   return (
     <>
@@ -18,10 +16,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm(show => !show)}>Add new
-          cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
